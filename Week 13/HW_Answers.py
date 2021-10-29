ae10fe5f54de0070dd43cf201d5fe8940b2cf94c
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2021-10-28 22:19:39 -0400

    Week 13 Fix

--- Week 13/HW_Answers.py
+++ Week 13/HW_Answers.py
@@ -4,7 +4,7 @@ def merge(arr1, arr2):
     p1 = p2 = 0
     res = []
     while p1 < len(arr1) or p2 < len(arr2):
-        if p1 < len(arr1) and (p2 == len(arr2) or arr2[p1] <= arr2[p2]):
+        if p1 < len(arr1) and (p2 == len(arr2) or arr1[p1] <= arr2[p2]):
             res.append(arr1[p1])
             p1 += 1
         else:
