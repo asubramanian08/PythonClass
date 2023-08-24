8bbd7d9992342d4dac6d2e144ae76450708103a9
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2023-08-23 21:12:06 -0400

    Week 68

--- 2048-Project/code.js
+++ 2048-Project/code.js
@@ -1,14 +1,26 @@
 const http = require('http');
+var fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+fs.readFile('index.html', function (err, html) {
+    if (err) throw err;
+    http.createServer(function (request, response) {
+        response.writeHeader(200, { "Content-Type": "text/html" });
+        response.write(html);
+        response.end();
+    }).listen(port);
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello World');
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+// server link: http://127.0.0.1:3000
