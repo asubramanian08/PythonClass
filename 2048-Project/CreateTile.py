12b369dfb7087564298166e8c1fc02e9f44360d2
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2023-09-29 21:50:11 -0400

    Week 72

--- 2048-Project/CreateTile.py
+++ 2048-Project/CreateTile.py
@@ -1,20 +1,26 @@
 # Generate a 2048 tile using Py2thon's PIL package
 
-import PIL
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 def createTile(number, color, file):
-
-    # Create the image
+   
+    # Create the background
     width = 200
-    hight = 200
+    height = 200
     img = Image.new('RGB', (width, height), color)
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("sans-serif.ttf", 46)
-    draw.text((x, y), "" + number + "",(r,g,b), font = font)
+
+    # Add and center text
+    text = str(number)
+    font = ImageFont.truetype("Arial.ttf", 200)
+    _, _, w, h = draw.textbbox((0, 0), text, font=font)
+    x = (width - w) / 2
+    y = (height - h) / 2
+    draw.text((x, y), text, fill=(0,0,0), font=font)
 
     # Save the image
     img.save(file)
 
-# To create a square image with the specified color and the number (in the center).
-# Save the image in the given file
-# createTile(2, "FFFF00", 'img/2.png')
+createTile(2, (255, 255, 0), 'img/2.png')
--- 2048-Project/gridMechanics.js
+++ 2048-Project/gridMechanics.js
@@ -1,9 +1,26 @@
 const { BlockList } = require("net");
 
+
 array = [[0, 0, 0, 0],
 [0, 0, 0, 0],
 [0, 0, 0, 0],
 [0, 0, 0, 0]];
+
+
+
+array =
+    [[0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 2, 2, 8],
+    [16, 4, 4, 32]];
+// -> when you swipe left
+array =
+    [[0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 4, 8],
+    [0, 16, 8, 32]];
+
+
 
 var score = 0;
 
@@ -16,6 +33,34 @@ window.addEventListener("keydown", (event) => { if (event.defaultPrevented) { re
 // swipedown
 window.addEventListener("keydown", (event) => { if (event.defaultPrevented) { return; } switch (event.key) { case "ArrowDown": } });
 
+
+function swipeleft() {
+
+
+    [0, 2, 2, 2]
+    [4, 2, 0, 0]
+
+    array = [[w, x, y, z], [w, x, y, z], [w, x, y, z], [w, x, y, z]]
+    for i in array[]{
+        if (w = x) {
+            w = w + x
+        }
+        newarry = [[w, x, y, z], [w, x, y, z], [w, x, y, z], [w, x, y, z]]
+        if (x = y) {
+            x = x + y
+        }
+        newarry = [[w, x, y, z], [w, x, y, z], [w, x, y, z], [w, x, y, z]]
+        if (y = z) {
+            y = y + z
+        }
+        newarry = [[w, x, y, z], [w, x, y, z], [w, x, y, z], [w, x, y, z]]
+    }
+    newarry = [[w, x, y, z], [w, x, y, z], [w, x, y, z], [w, x, y, z]]
+
+
+    array = newarray
+
+}
 
 function after_every_move() {
     // update the score
