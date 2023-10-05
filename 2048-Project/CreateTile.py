6b22de991a49ee3e1cb4bd7762bd746573d1fda5
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2023-10-04 21:29:47 -0400

    Week 73

--- 2048-Project/CreateTile.py
+++ 2048-Project/CreateTile.py
@@ -15,12 +15,22 @@ def createTile(number, color, file):
     # Add and center text
     text = str(number)
     font = ImageFont.truetype("Arial.ttf", 200)
-    _, _, w, h = draw.textbbox((0, 0), text, font=font)
-    x = (width - w) / 2
-    y = (height - h) / 2
+
+    temp1, temp2, w, h = draw.textbbox((0, 0), text, font=font)
+    x = (width - (w + temp1)) // 2
+    y = (height - (h + temp2)) // 2
     draw.text((x, y), text, fill=(0,0,0), font=font)
 
     # Save the image
     img.save(file)
 
-createTile(2, (255, 255, 0), 'img/2.png')
+
+# Idea for changing colors
+# 1. You have a set "step length" of distance away from the previous hue and saturation
+# 2. Saturation will start at 10 and decrease by .5 every time
+# 3. The hue will be such that the distance is 10: hue = sqrt(10^2 - value^2)
+
+x = 2
+while x <= 2048:
+    createTile(x, (255, 255, 0), f'img/{x}.png')
+    x *= 2
