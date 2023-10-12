bca1d6655cb464255defc29ef7382ceb0162ebec
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2023-10-11 21:04:41 -0400

    Week 74

--- 2048-Project/CreateTile.py
+++ 2048-Project/CreateTile.py
@@ -1,20 +1,29 @@
-# Generate a 2048 tile using Py2thon's PIL package
+# Generate a 2048 tile using Python's PIL package
 
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+
+SIZES = [200, 150, 110, 85]
+
+def digitCount(x):
+    digits = 1
+    while x // 10 > 0:
+        x //= 10
+        digits += 1
+    return digits
 
 def createTile(number, color, file):
    
     # Create the background
     width = 200
     height = 200
-    img = Image.new('RGB', (width, height), color)
+    img = Image.new('HSV', (width, height), color) # 'RGB'
     draw = ImageDraw.Draw(img)
 
     # Add and center text
     text = str(number)
-    font = ImageFont.truetype("Arial.ttf", 200)
+    font = ImageFont.truetype("Arial.ttf", SIZES[digitCount(number) - 1])
 
     temp1, temp2, w, h = draw.textbbox((0, 0), text, font=font)
     x = (width - (w + temp1)) // 2
@@ -27,10 +36,22 @@ def createTile(number, color, file):
 
 # Idea for changing colors
 # 1. You have a set "step length" of distance away from the previous hue and saturation
-# 2. Saturation will start at 10 and decrease by .5 every time
-# 3. The hue will be such that the distance is 10: hue = sqrt(10^2 - value^2)
+# 2. Saturation jump will start at 10 and decrease by .5 every time
+# 3. The hue will be such that the distance is 10: hue = sqrt(10^2 - saturation^2)
 
-x = 2
+# HSV -> RGB
+
+x = 2 
 while x <= 2048:
+    H = 66
+    S = 40
+    V= 100
+    HD = 2
+    VD = 10
+    while VD > 0 and HD > 0:
+        VD = VD - 0.5
+        HD = HD + 1
+
+
     createTile(x, (255, 255, 0), f'img/{x}.png')
     x *= 2
