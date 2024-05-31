98c09065e0a0ac6b4dc31b81aec4eece59fceb9f
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2024-05-30 21:01:31 -0400

    Week 90

--- 2048-Project/code.js
+++ 2048-Project/code.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-fs.readFile('clicker_index.html', function (err, html) {
+fs.readFile('index.html', function (err, html) {
     if (err) throw err;
     http.createServer(function (request, response) {
         response.writeHeader(200, { "Content-Type": "text/html" });
