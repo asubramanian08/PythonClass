e296eb1998c24a8007cd6b601bf68d9bc4a46cd4
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2024-06-05 21:30:19 -0400

    Week 91

--- 2048-Project/code.js
+++ 2048-Project/code.js
@@ -4,27 +4,17 @@ var fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-fs.readFile('index.html', function (err, html) {
-    if (err) throw err;
-    http.createServer(function (request, response) {
-        response.writeHeader(200, { "Content-Type": "text/html" });
-        response.write(html);
-        response.end();
-    }).listen(port);
-});
+http.createServer(function (request, response) {
+
+    response.writeHeader(200);
+    let filename = "." + request.url;
+    if (request.url === "/") {
+        filename = "./index.html";
+    }
+    let fileContents = fs.readFileSync(filename);
+    response.write(fileContents);
+    response.end();
+}).listen(port);
 
 // terminal: node code.js
-
-// const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.end('Hello World');
-// });
-
-// server.listen(port, hostname, () => {
-//     console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
 // server link: http://127.0.0.1:3000
-
-// https://localhost:3000/
