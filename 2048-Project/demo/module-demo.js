f7b4efc44ea84835547f1089316a2912189e5f22
Arjun Subramanian <arjun.subramanian.08@gmail.com>
2024-09-18 04:45:11 -0400

    Week 93

---
+++ 2048-Project/demo/module-demo.js
@@ -0,0 +1,10 @@
+// var http = require('http');
+// var url = require('url');
+
+exports.add = function (a, b) {
+    var x = a + b;
+    return x;
+}
+
+
+// http://localhost:8080/?var=<br>On-a-new-line
--- 2048-Project/demo/node-demo.js
+++ 2048-Project/demo/node-demo.js
@@ -1,10 +1,10 @@
 var http = require('http');
 var url = require('url');
+var add = require('./module-demo.js');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('Hello World!');
-    res.end(url.parse(req.url, true).query.name);
+    var q = url.parse(req.url, true).query;
+    res.end(add.add(q.a, q.b).toString()); // http://localhost:8080/?a=4&b=4
 }).listen(8080);
-
-// http://localhost:8080/?var=<br>On-a-new-line
